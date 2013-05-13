13f37522d4454a115e6f14822654f7a4866a7245
Karthik Dhore <karthik.dhore@gmail.com>
2013-05-13 15:16:08 -0400

    generate truths and priors

---
+++ generateTruth.py
@@ -0,0 +1,62 @@
+"""
+Building codes:
+Frist = 0, Nassau = 1, Whitman = 2, Forbes  3, Sherred = 4, Friend = 5, Lewis = 6
+
+"""
+import logging, math, random
+
+import numpy as nm
+
+nbuildings = 7;
+
+Map = {'0': (0,0),'1': (-1,4), '2': (-2,-2),'3' : (-6,-2),'4': (3,2), '5': (3,3.5), '6': (3, -2)};
+
+for i in range(0, nbuildings):
+    for j in range(0, nbuildings):
+        point1 = nm.asarray(Map[str(i)])
+        point2 = nm.asarray(Map[str(j)])
+        slowtruth = nm.linalg.norm(point1-point2)*2.5
+        if (slowtruth != 0):
+            slowtruth += random.gauss(4, 10)
+            if (slowtruth < 0):
+                slowtruth = math.fabs(slowtruth);
+        variance = random.gauss(8, 15);
+        if (variance < 0):
+            variance = math.fabs(variance);
+        if (slowtruth == 0):
+            variance = 0;
+        print i, j, "%.2f" % round(slowtruth,2), "%.2f" % round(variance,2)
+        
+for i in range(0, nbuildings):
+    for j in range(0, nbuildings):
+        point1 = nm.asarray(Map[str(i)])
+        point2 = nm.asarray(Map[str(j)])
+        mediumtruth = nm.linalg.norm(point1-point2)*2.5
+        if (mediumtruth != 0):
+            mediumtruth += random.gauss(0, 10)
+            if (mediumtruth < 0):
+                mediumtruth = math.fabs(mediumtruth);
+        variance = random.gauss(8, 15);
+        if (variance < 0):
+            variance = math.fabs(variance);
+        if (slowtruth == 0):
+            variance = 0;
+        print i, j, "%.2f" % round(mediumtruth,2), "%.2f" % round(variance,2)
+        
+for i in range(0, nbuildings):
+    for j in range(0, nbuildings):
+        point1 = nm.asarray(Map[str(i)])
+        point2 = nm.asarray(Map[str(j)])
+        fasttruth = nm.linalg.norm(point1-point2)*2.5
+        if (fasttruth != 0):
+            fasttruth -= random.gauss(2, 10)
+            if (fasttruth < 0):
+                fasttruth = math.fabs(fasttruth);
+        variance = random.gauss(8, 15);
+        if (variance < 0):
+            variance = math.fabs(variance);
+        if (fasttruth == 0):
+            variance = 0;
+        print i, j, "%.2f" % round(fasttruth,2), "%.2f" % round(variance,2)
+    
+    
--- generatepriors.py
+++ generatepriors.py
@@ -1,24 +1,40 @@
 """
 Building codes:
-Frist = 1, Nassau = 2, Whitman = 3, Fobes  4, Sherred = 5, Friend = 6, Lewis = 7
+Frist = 0, Nassau = 1, Whitman = 2, Forbes  3, Sherred = 4, Friend = 5, Lewis = 6
 
 """
 import logging, math, random
 
 import numpy as nm
 
-Map = {'1': (0,0),'2': (-1,4), '3': (-2,-2),'4' : (-6,-2),'5': (3,2), '6': (3,3.5), '7': (3, -2)};
-distancePrior = [[0 for x in xrange(7)] for x in xrange(7)];
-for i in range(1, 8):
-    for j in range(1, 8):
+nbuildings = 7;
+
+Map = {'0': (0,0),'1': (-1,4), '2': (-2,-2),'3' : (-6,-2),'4': (3,2), '5': (3,3.5), '6': (3, -2)};
+
+for i in range(0, nbuildings):
+    for j in range(0, nbuildings):
         point1 = nm.asarray(Map[str(i)])
         point2 = nm.asarray(Map[str(j)])
-        dist = nm.linalg.norm(point1-point2)
-        distancePrior[i-1][j-1] = dist
-nm.set_printoptions(precision=3)
-for i in range(0, 7):
-    for j in range(0, 7):
-        print (distancePrior[i][j]),
-    print;
+        slowdist = nm.linalg.norm(point1-point2)*2.5
+        if (slowdist != 0):
+            slowdist += 4 
+        print i, j, "%.2f" % round(slowdist,2)
+        
+for i in range(0, nbuildings):
+    for j in range(0, nbuildings):
+        point1 = nm.asarray(Map[str(i)])
+        point2 = nm.asarray(Map[str(j)])
+        mediumdist = nm.linalg.norm(point1-point2)*2.5
+        print i, j, "%.2f" % round(mediumdist,2)
+        
+for i in range(0, nbuildings):
+    for j in range(0, nbuildings):
+        point1 = nm.asarray(Map[str(i)])
+        point2 = nm.asarray(Map[str(j)])
+        fastdist = nm.linalg.norm(point1-point2)*2.5
+        if (fastdist != 0):
+            fastdist -= 2 
+        print i, j, "%.2f" % round(fastdist,2)
+        
     
     
